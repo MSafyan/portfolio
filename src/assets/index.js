9eb5a4fa819b181e7c4e9b1c50954de8ed95f9c8
MSafyan <msafyan080@gmail.com>
2026-04-24 11:36:08 +0500

    Update asset formats and clean up unused files: Replaced PNG assets with WEBP format for better performance, updated Tailwind configuration for the hero background, and removed several unused image files from the project to streamline the asset directory.

--- src/assets/index.js
+++ src/assets/index.js
@@ -16,100 +16,25 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import fawkes from "./fawkess.webp";
-import cloth from "./cloth.png";
+import cloth from "./cloth.webp";
 import chezchef from "./chez.webp";
-import alpha from "./al1.png";
-import techaccess from "./tech.webp";
-import wind from "./wind.png";
-import xotics from "./xotics.png";
+import alpha from "./al1.webp";
+import wind from "./wind.webp";
+import xotics from "./xotics.webp";
 import pashma from "./pp.webp";
-import driver from "./driver.png";
-import consumer from "./consumer.png";
-import attendance from "./attendance.png";
-import kidsque from "./kidsque.png";
-import royal from "./royal.png";
-import vendors from "./vendors.png";
-import kids from "./kids.jpeg";
-import splashx from "./splashx.png";
-import bet from "./bet.png";
-import hoboetch from "./ht.png";
-import digitalAimz from "./da.png";
-import megaverses from "./megaverse.png";
-import flutter from "./flutter.png";
-import dart from "./dart.png";
-import firebase from "./firebase.png";
-import getx from "./getx.png";
+import bet from "./bet.webp";
 import myLogo from "./mlogo.png";
-import climateIq from "./climateIq.png";
-import wireguardVpn from "./wireguardVpn.jpg";
-import hanti from "./hanti.png";
-import ac from "./attendance/ac.png";
-import ah from "./attendance/ah.png";
-import ahh from "./attendance/ahh.png";
-import al from "./attendance/al.png";
-import all from "./attendance/all.png";
-import otp from "./attendance/otp.png";
-import splas from "./attendance/splas.png";
-import de from "./driver/de.png";
-import den from "./driver/den.png";
-import dg from "./driver/dg.png";
-import dh from "./driver/dh.png";
-import dif from "./driver/dif.png";
-import doo from "./driver/do.png";
-import dr from "./driver/dr.png";
-import dp from "./driver/dp.png";
-import ds from "./driver/ds.png";
-import dvs from "./driver/dvs.png";
-import alle from "./consumer/alle.png";
-import cs from "./consumer/cs.png";
-import dashc from "./consumer/dashc.png";
-import not from "./consumer/not.png";
-import onb from "./consumer/onb.png";
-import cat from "./climateIq/cat.png";
-import control from "./climateIq/control.png";
-import fan from "./climateIq/fan.png";
-import navigation from "./climateIq/navigation.png";
-import net from "./climateIq/net.png";
-import sched from "./climateIq/sched.png";
-import stats from "./climateIq/stats.png";
-import Timer from "./climateIq/Timer.png";
-import SplashScreen from "./climateIq/SplashScreen.png";
-import cal from "./hanti/cal.png";
-import chat from "./hanti/chat.png";
-import inbo from "./hanti/inbo.png";
-import listing from "./hanti/listing.png";
-import splash from "./hanti/splash.png";
-import varr from "./hanti/var.png";
-import payment from "./hanti/payment.png";
-import vpn from "./vpn/vpn.png";
-import active from "./vpn/active.png";
-import locations from "./vpn/locations.png";
-import signup from "./vpn/signup.png";
-import login from "./vpn/Login.png";
-import networks from "./vpn/networks.png";
-import royalfood from "./royalfood.png";
-import p1 from "./pakque/p1.jpg";
-import p2 from "./pakque/p2.jpg";
-import p3 from "./pakque/p3.jpg";
-import p4 from "./pakque/p4.jpg";
-import p5 from "./pakque/p5.jpg";
-import p6 from "./pakque/p6.jpg";
-import p7 from "./pakque/p7.jpg";
-import p8 from "./pakque/p8.jpg";
-import p9 from "./pakque/p9.jpg";
-import p10 from "./pakque/p10.jpg";
-import p11 from "./pakque/p11.jpg";
-import x1 from "./xotics/x1.png";
-import x2 from "./xotics/x2.png";
-import x3 from "./xotics/x3.png";
-import x4 from "./xotics/x4.png";
-import x5 from "./xotics/x5.jpg";
-import x6 from "./xotics/x6.png";
-import x7 from "./xotics/x7.png";
-import x8 from "./xotics/x8.png";
-import x9 from "./xotics/x9.png";
-import x10 from "./xotics/x10.png";
-import x11 from "./xotics/x11.png";
+import x1 from "./xotics/x1.webp";
+import x2 from "./xotics/x2.webp";
+import x3 from "./xotics/x3.webp";
+import x4 from "./xotics/x4.webp";
+import x5 from "./xotics/x5.webp";
+import x6 from "./xotics/x6.webp";
+import x7 from "./xotics/x7.webp";
+import x8 from "./xotics/x8.webp";
+import x9 from "./xotics/x9.webp";
+import x10 from "./xotics/x10.webp";
+import x11 from "./xotics/x11.webp";
 import helpicon from "./helpicon.png";
 import h1 from "./helpicon/h1.webp";
 import h2 from "./helpicon/h2.webp";
@@ -129,73 +54,32 @@ import r6 from "./resihop/r6.webp";
 import r7 from "./resihop/r7.webp";
 import r8 from "./resihop/r8.webp";
 import r9 from "./resihop/r9.webp";
-import s1 from "./superheroesim/s1.webp";
-import s2 from "./superheroesim/s2.webp";
-import s3 from "./superheroesim/s3.webp";
-import s4 from "./superheroesim/s4.webp";
-import s5 from "./superheroesim/s5.jpeg";
-import b1 from "./bahria/b1.webp";
-import b2 from "./bahria/b2.webp";
-import b3 from "./bahria/b3.webp";
-import b4 from "./bahria/b4.webp";
-import b5 from "./bahria/b5.webp";
-import b6 from "./bahria/b6.webp";
-import b7 from "./bahria/b7.webp";
-import tm1 from "./tingme/tm1.webp";
-import tm2 from "./tingme/tm2.webp";
-import tm3 from "./tingme/tm3.webp";
-import tm4 from "./tingme/tm4.webp";
-import tm5 from "./tingme/tm5.webp";
-import tm6 from "./tingme/tm6.webp";
-import tm7 from "./tingme/tm7.webp";
-import tm8 from "./tingme/tm8.webp";
-import t1 from "./tinghot/t1.webp";
-import t2 from "./tinghot/t2.webp";
-import t3 from "./tinghot/t3.webp";
-import t4 from "./tinghot/t4.webp";
-import t5 from "./tinghot/t5.webp";
-import t6 from "./tinghot/t6.webp";
-import t7 from "./tinghot/t7.webp";
-import successIcon from "./suc.png";
-import failureIcon from "./fai.png";
-import closeIcon from "./clo.png";
 import playStoreIcon from "./playstore.webp";
 import appStoreIcon from "./appstore.webp";
 import webIcon from "./web.webp";
-import l1 from "./l1.png";
-import w1 from "./w1.webp";
-import port from "./port.png";
-
-import rescue1 from "./rescue/rescue1.webp";
-import rescue2 from "./rescue/rescue2.webp";
-import rescue3 from "./rescue/rescue3.webp";
-import rescue4 from "./rescue/rescue4.webp";
-import rescue5 from "./rescue/rescue5.webp";
-import rescue6 from "./rescue/rescue6.webp";
+import port from "./port.webp";
 import myResume from "./resume/resume.pdf";
-import legacy1 from "./legacy1.png";
-import legacy2 from "./legacy2.png";
-import legacy3 from "./legacy3.png";
-import legacy4 from "./legacy4.png";
-import palmetto1 from "./palmetto1.png";
-import palmetto2 from "./palmetto2.png";
-import palmetto3 from "./palmetto3.png";
-import pinkchicken1 from "./pinkchicken1.png";
-import pinchicken2 from "./pinchicken2.png";
-import erma from "./erma.png";
-import terraform from "./terraform.png";
-import udemyAWS from "./udemyAWS.png";
-import udemyCKAD from "./udemyCKAD.png";
-import kubernetesCert from "./resume/kubernetesCert.png";
-import wesion from "./wesion.png";
-import wesion2 from "./wesion2.png";
-import wesion3 from "./wesion3.png";
-import cvr1 from "./cvr1.png";
-import cvr2 from "./cvr2.png";
-import cvr3 from "./cvr3.png";
-import cvr4 from "./cvr4.png";
-import cvr5 from "./cvr5.png";
-import cvr6 from "./cvr6.png";
+import legacy1 from "./legacy1.webp";
+import legacy2 from "./legacy2.webp";
+import legacy3 from "./legacy3.webp";
+import legacy4 from "./legacy4.webp";
+import palmetto1 from "./palmetto1.webp";
+import palmetto2 from "./palmetto2.webp";
+import palmetto3 from "./palmetto3.webp";
+import pinkchicken1 from "./pinkchicken1.webp";
+import pinchicken2 from "./pinchicken2.webp";
+import erma from "./erma.webp";
+import terraform from "./terraform.webp";
+import kubernetesCert from "./resume/kubernetesCert.webp";
+import wesion from "./wesion.webp";
+import wesion2 from "./wesion2.webp";
+import wesion3 from "./wesion3.webp";
+import cvr1 from "./cvr1.webp";
+import cvr2 from "./cvr2.webp";
+import cvr3 from "./cvr3.webp";
+import cvr4 from "./cvr4.webp";
+import cvr5 from "./cvr5.webp";
+import cvr6 from "./cvr6.webp";
 
 export {
   logo,
@@ -219,100 +103,11 @@ export {
   cloth,
   chezchef,
   alpha,
-  techaccess,
   wind,
   xotics,
   pashma,
-  driver,
-  consumer,
-  attendance,
-  kidsque,
-  royal,
-  vendors,
-  kids,
-  splashx,
   bet,
-  hoboetch,
-  digitalAimz,
-  megaverses,
-  flutter,
-  dart,
-  firebase,
-  getx,
   myLogo,
-  climateIq,
-  hanti,
-  wireguardVpn,
-
-  //seperate assets dha
-  ac,
-  ah,
-  ahh,
-  al,
-  all,
-  otp,
-  splas,
-  de,
-  den,
-  dg,
-  dh,
-  dif,
-  doo,
-  dr,
-  dp,
-  ds,
-  dvs,
-  alle,
-  cs,
-  dashc,
-  not,
-  onb,
-  //seperate assets dha
-  //Climate IQ
-  cat,
-  control,
-  fan,
-  navigation,
-  net,
-  sched,
-  stats,
-  Timer,
-  SplashScreen,
-  //Climate IQ
-  //Hanti
-  cal,
-  chat,
-  inbo,
-  listing,
-  splash,
-  varr,
-  payment,
-  //Hanti
-  //VPN
-  vpn,
-  active,
-  locations,
-  signup,
-  login,
-  networks,
-  //VPN
-  //Royal Food
-  royalfood,
-  //Royal Food
-  //Pakque
-  p1,
-  p2,
-  p3,
-  p4,
-  p5,
-  p6,
-  p7,
-  p8,
-  p9,
-  p10,
-  p11,
-  //Pakque
-  //Xotics
   x1,
   x2,
   x3,
@@ -324,7 +119,6 @@ export {
   x9,
   x10,
   x11,
-  //Xotics
   helpicon,
   h1,
   h2,
@@ -334,6 +128,7 @@ export {
   h6,
   h7,
   h8,
+  helpiWeb,
   r1,
   r2,
   r3,
@@ -343,60 +138,11 @@ export {
   r7,
   r8,
   r9,
-  s1,
-  s2,
-  s3,
-  s4,
-  s5,
-  b1,
-  b2,
-  b3,
-  b4,
-  b5,
-  b6,
-  b7,
-  tm1,
-  tm2,
-  tm3,
-  tm4,
-  tm5,
-  tm6,
-  tm7,
-  tm8,
-  t1,
-  t2,
-  t3,
-  t4,
-  t5,
-  t6,
-  t7,
-  successIcon,
-  failureIcon,
-  closeIcon,
   playStoreIcon,
   appStoreIcon,
   webIcon,
-  l1,
-  w1,
   port,
-  rescue1,
-  rescue2,
-  rescue3,
-  rescue4,
-  rescue5,
-  rescue6,
-  helpiWeb,
   myResume,
-  // mapped public assets
-  wesion,
-  wesion2,
-  wesion3,
-  cvr1,
-  cvr2,
-  cvr3,
-  cvr4,
-  cvr5,
-  cvr6,
   legacy1,
   legacy2,
   legacy3,
@@ -408,7 +154,14 @@ export {
   pinchicken2,
   erma,
   terraform,
-  udemyAWS,
-  udemyCKAD,
   kubernetesCert,
+  wesion,
+  wesion2,
+  wesion3,
+  cvr1,
+  cvr2,
+  cvr3,
+  cvr4,
+  cvr5,
+  cvr6,
 };
